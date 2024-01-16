5cd4d3640873d80298f0c657d5468a0a1b2d2397
Ali Meral <meralmuhammet544@gmail.com>
2024-01-16 18:05:33 +0300

    connect to databaste

--- api/server.js
+++ api/server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use("/", (req, res) => {
   res.send("heloo server");
