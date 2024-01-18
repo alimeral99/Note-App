e9e348b654ef475f88badb87654e730296e711fe
Ali Meral <meralmuhammet544@gmail.com>
2024-01-18 17:44:13 +0300

    add note in controlles

--- api/controllers/note.controllers.js
+++ api/controllers/note.controllers.js
@@ -2,9 +2,21 @@ const Note = require("../models/note");
 
 const getAllNote = async (req, res) => {
   const notes = await Note.find({});
+
   res.status(200).json(notes);
+};
+
+const addNote = async (req, res) => {
+  try {
+    const note = await new Note(req.body);
+    await note.save();
+    res.status(400).json({ note });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
   getAllNote,
+  addNote,
 };
--- api/routes/note.js
+++ api/routes/note.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { getAllNote } = require("../controllers/note.controllers");
+const { getAllNote, addNote } = require("../controllers/note.controllers");
 
 router.get("/", getAllNote);
+router.post("/", addNote);
 
 module.exports = router;
